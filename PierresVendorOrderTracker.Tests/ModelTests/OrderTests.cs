5c59a8897d851d61b0933fcaf3badf2c55a7f080
Tim Roth <timdroth@gmail.com>
2021-09-25 21:54:32 -0700

    create basic testing framework

---
+++ PierresVendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
---
+++ PierresVendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
