638f66876b489968c1f21bf0e14262b62f6b94a3
Tim Roth <timdroth@gmail.com>
2021-10-03 21:20:39 -0700

    add Vendor test cleanup code

--- PierresVendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ PierresVendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,9 +1,19 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using System;
-// using System.Collections.Generic;
-// using VendorOrderTracker.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
 
-// namespace VendorOrderTracker.Tests
-// {
-//   [TestClass]
-//   public class VendorTests
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    private Vendor _vendorObject;
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+      Vendor.ClearVendors();
+    }
+  }
+}
